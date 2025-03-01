8fbefdd4a6261fbe15ec73f2cd42a0ae29e61c58
dmpinero <dmpinero@gmail.com>
2025-03-01 14:00:02 +0100

    Update index.js and remove test-custom-action.yml

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-import core from '@actions/core';
-import github from '@actions/github';
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 try {
   const commodity = core.getInput('commodity');
-  console.log(`Getting current ${commodity} price per ounce...`);
-  let price = 0;
-  if (commodity.toLocaleLowerCase() === 'gold') {
-    price = 2019.80;
-    console.log(`Current gold price per ounce: ${price} USD`);
-  } else if (commodity.toLocaleLowerCase() === 'silver') {
-    price = 24.07;
-    console.log(`Current silver price per ounce: ${price} USD`);
+  if (commodity.toLowerCase() !== 'gold' && commodity.toLowerCase() !== 'silver') {
+    throw new Error(`Commodity ${commodity} is not valid`);
   }
+  const currency = core.getInput('currency');
+  if (currency.toUpperCase() !== 'USD' && currency.toUpperCase() !== 'EUR') {
+    throw new Error(`Currency ${currency} is not valid`);
+  }
+  console.log(`Getting current ${commodity} price per ounce in ${currency}...`);
+  let price = 0;
+  if (commodity.toLowerCase() === 'gold') {
+    price = currency.toUpperCase() === 'USD' ? 2652.84 : 2524.52;
+  } else {
+    price = currency.toUpperCase() === 'USD' ? 30.58 : 29.10;
+  }
+  console.log(`Current ${commodity} price per ounce: ${price} ${currency}`);
 
   core.setOutput('price', price);
 
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  console.log(`Event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
